6767197cd8913b8a27a2cee0fd92020ae6ee1b91
stock-user <11197256@qq.com>
2026-02-27 07:48:48 +0800

    fix_cron_config

--- instock/bin/run_cron.sh
+++ instock/bin/run_cron.sh
@@ -6,8 +6,18 @@ export PYTHONPATH=/data/InStock
 export LC_CTYPE=zh_CN.UTF-8
 
 # 环境变量输出
-# https://stackoverflow.com/questions/27771781/how-can-i-access-docker-set-environment-variables-from-a-cron-job
 printenv | grep -v "no_proxy" >> /etc/environment
 
-#启动cron服务。在前台
+# 确保crontab配置正确
+echo "SHELL=/bin/sh
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+# min hour day month weekday command
+*/30 9,10,11,13,14,15 * * 1-5 /bin/run-parts /etc/cron.hourly
+30 17 * * 1-5 /bin/run-parts /etc/cron.workdayly
+30 10 * * 3,6 /bin/run-parts /etc/cron.monthly
+" > /var/spool/cron/crontabs/root
+
+chmod 600 /var/spool/cron/crontabs/root
+
+# 启动cron服务。在前台
 /usr/sbin/cron -f
